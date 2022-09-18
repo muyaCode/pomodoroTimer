090b9f9d3e144f7bca1ab950643914adec3c72fb
muyi <38781672@qq.com>
2022-09-18 23:53:13 +0800

    style：格式化代码

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const createWindow = () => {
     webPreferences: {
       // preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: false  //  require报错解决
+      contextIsolation: false // 解决require报错
     }
   })
 
